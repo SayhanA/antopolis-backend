ac21a129980f29eaca82d9fd362d5fc265403c73
Sayhan Ahmed <sayhan@ewnbd.com>
2025-06-28 20:04:13 +0600

    resolving deployment issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const foodRoutes = require("./routes/foodRoutes");
-const categoryRoutes = require("./routes/categoryRoutes")
+const categoryRoutes = require("./routes/categoryRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -12,11 +12,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+app.use('/', (req, res, next) => {
+  res.send('hello world.')
+})
 app.use("/api/foods", foodRoutes);
 app.use("/api/categories", categoryRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@sayhan.fatp7.mongodb.net/test?retryWrites=true&w=majority&appName=Sayhan`
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
