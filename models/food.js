08e63fd099abeb5985e44671fef6887a79030d28
Sayhan Ahmed <sayhan@ewnbd.com>
2025-06-28 22:27:37 +0600

    food model change to update data

--- models/food.js
+++ models/food.js
@@ -4,6 +4,8 @@ const foodSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
   image: { type: String },
+  rating: {type:Number, default: 1},
+  price:  {type:Number, default: 320},
   createdAt: { type: Date, default: Date.now() },
 });
 
